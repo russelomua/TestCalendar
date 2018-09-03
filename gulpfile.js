b9245d75a8701b938dc2c431b99132961d4c6949
Николай <russel.omua@gmail.com>
2018-09-04 00:28:22 +0300

    first step of development
    Calendar view generation, next/prev changing. spend 3 hours for this stuff.

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+	concat = require('gulp-concat'),
+	connect = require('gulp-connect'),
+	rimraf = require('rimraf');
+
+var path = {
+	build: {
+		base: "dist/",
+		js: "dist/js/",
+		css: "dist/css/",
+		fonts: "dist/fonts/"
+	},
+	src: {
+		html: "src/index.html",
+		js: [
+			"src/js/app.js"
+		],
+		css: [
+			"src/css/app.css",
+			'node_modules/bootstrap/dist/css/bootstrap.css'
+		],
+		lib: [
+			"node_modules/angular/angular.min.js",
+			"node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js",
+			"node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
+			"node_modules/angular-i18n/angular-locale_ru-ua.js"
+		],
+		fonts: "node_modules/bootstrap/dist/fonts/*"
+	},
+	watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+		html: 'src/index.html',
+		js: 'src/**/*.js',
+		css: 'src/css/**/*.+(css)'
+    },
+    clean: 'dist'
+}
+
+gulp.task('lib', function () {
+	//Compile libs to one file
+	return gulp.src(path.src.lib)
+		.pipe(concat('lib.js'))
+		.pipe(gulp.dest(path.build.js))
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+	return gulp.src(path.src.js)
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest(path.build.js))
+		.pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+    return gulp.src(path.src.css, {base: path.build.css})
+		.pipe(concat('app.css'))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(connect.reload()); //server reload
+});
+
+gulp.task('html', function () {
+    return gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.base))
+        .pipe(connect.reload()); //server reload
+});
+
+gulp.task('fonts', function () {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+        .pipe(connect.reload()); //server reload
+});
+
+gulp.task('watch', function(){
+	gulp.watch(path.watch.html, gulp.parallel('html'));
+	gulp.watch(path.watch.js, gulp.parallel('js'));
+	gulp.watch(path.watch.css, gulp.parallel('css'));
+});
+
+gulp.task('clean', function (cb) {
+    return rimraf(path.clean, cb);
+});
+
+gulp.task('connect', function () {
+    return connect.server({
+		root: path.build.base,
+		livereload: true,
+        host: "0.0.0.0",
+		port: 3000,
+        fallback: path.build.base+'index.html'
+	});
+});
+
+gulp.task('build', gulp.series(['lib', 'js', 'css', 'html', 'fonts' ]));
+
+gulp.task('default', gulp.parallel(['build', 'connect', 'watch']));
